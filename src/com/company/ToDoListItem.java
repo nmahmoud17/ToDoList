6ec4a058719779f00335ea679f31032574b9035a
NM17 <nada.mahmoud@detroitlabs.com>
2019-01-31 17:54:08 -0500

    creating getters and setters for toDoList Item

--- src/com/company/ToDoListItem.java
+++ src/com/company/ToDoListItem.java
@@ -1,4 +1,32 @@
 package com.company;
 
 public class ToDoListItem {
+
+    private String itemName;
+    private String itemState;
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public String getItemState() {
+        return itemState;
+    }
+
+    public void setItemState(String itemState) {
+        this.itemState = itemState;
+    }
+
+    public ToDoListItem(String itemName, String itemState) {
+        this.itemName = itemName;
+        this.itemState = itemState;
+
+    }
+
+
+
 }
